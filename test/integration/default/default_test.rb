19f641f8740ed6fefefbc27a1a29a10f7520b4da
Filip Dimitrov <fdimitrov@Filips-MacBook-Pro.local>
2018-09-26 15:31:58 +0300

    fixed the rate limiting problem

--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -1,6 +1,9 @@
 control 'check_website' do
 
-  describe http('https://api.github.com/repos/VisitBG/example-bananas') do
+# This seems to fail due to rate limiting
+#  describe http('https://api.github.com/repos/VisitBG/example-bananas') do
+
+  describe http('https://github.com/VisitBG/example-bananas') do
     its('status') { should cmp 200 }
     its('body') { should match 'My awesome codebase' }
   end
